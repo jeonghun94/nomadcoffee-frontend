a5e41b38273fc4a954b06ce99fcde56779e2f1ef
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-04-12 10:52:58 +0900

    react hook form 로그인, 회원가입 페이지에 추가

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import { isLoggedInVar } from "../apollo";
+// import { isLoggedInVar } from "../apollo";
 import Bottom from "../components/auth/Bottom";
 import Layout from "../components/auth/Layout";
 import Separator from "../components/auth/Sperator";
@@ -8,16 +8,57 @@ import Title from "../components/auth/Title";
 import Input from "../components/auth/Input";
 import Form from "../components/auth/Form";
 import routes from "../routes";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  };
   return (
     <Layout>
       <PageTitle title="Login | Nomad-Coffee" />
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit, onError)}>
         <Title title="Welcome To" subTitle="Nomad-Coffee" />
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
-        <Button onClick={() => isLoggedInVar(true)}>Login</Button>
+        <Input
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+            // minLength: {
+            //   value: 3,
+            //   message: "유저의 이름은 3글자 이상이어야 합니다.",
+            // },
+          })}
+          onChange={() => clearErrors("username")}
+          placeholder="Username"
+        />
+        {errors.username?.message}
+        <Input
+          {...register("password", {
+            required: {
+              value: true,
+              message: "password is required",
+            },
+          })}
+          onChange={() => clearErrors("password")}
+          placeholder="Password"
+        />
+        {errors.password?.message}
+        <Button>Login</Button>
         <Separator />
         <Bottom
           text="Don't have an account?"
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -8,19 +8,92 @@ import Title from "../components/auth/Title";
 import Input from "../components/auth/Input";
 import Form from "../components/auth/Form";
 import routes from "../routes";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  };
   return (
     <Layout>
       <PageTitle title="Sign-Up | Nomad-Coffee" />
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit, onError)}>
         <Title title="Sign Up" subTitle="Nomad-Coffee" />
-        <Input placeholder="Frist Name" />
-        <Input placeholder="Last Name" />
-        <Input placeholder="Email" />
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
-        <Button>Sign Up</Button>
+        <Input
+          {...register("firstName", {
+            required: {
+              value: true,
+              message: "firstName is required",
+            },
+            maxLength: {
+              value: 3,
+              message: "firstName is 3 characters short",
+            },
+          })}
+          onChange={() => clearErrors("firstName")}
+          placeholder="Frist Name"
+        />
+        {errors.firstName?.message}
+        <Input
+          {...register("lastName", {
+            required: {
+              value: true,
+              message: "lastName is required",
+            },
+          })}
+          onChange={() => clearErrors("lastName")}
+          placeholder="Last Name"
+        />
+        {errors.lastName?.message}
+        <Input
+          {...register("email", {
+            required: {
+              value: true,
+              message: "email is required",
+            },
+          })}
+          onChange={() => clearErrors("email")}
+          placeholder="Email"
+        />
+        {errors.email?.message}
+        <Input
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+          })}
+          onChange={() => clearErrors("username")}
+          placeholder="Username"
+        />
+        {errors.username?.message}
+        <Input
+          {...register("password", {
+            required: {
+              value: true,
+              message: "password is required",
+            },
+            minLength: {
+              value: 6,
+              message: "password is 6 characters long",
+            },
+          })}
+          onChange={() => clearErrors("password")}
+          placeholder="Password"
+        />
+        {errors.password?.message}
+        <Button onClick={() => isLoggedInVar(true)}>Sign Up</Button>
         <Separator />
         <Bottom
           text="Do you have an account?"
